df99f074b18f2603ccd8b20458686246d3817d37
zhongjun <zhongjun96@aliyun.com>
2020-07-09 10:41:38 +0800

    ValidParentheses

--- src/com/zhongjun/leetcode/LongestCommonPrefix.java
+++ src/com/zhongjun/leetcode/LongestCommonPrefix.java
@@ -43,13 +43,14 @@ public class LongestCommonPrefix {
             char c = strs[0].charAt(i);
             //比较第 1,2,3... 行的字符和第 0 行是否相等
             for (int j = 1; j < strs.length; j++) {
-                /**
-                 * i == strs[j].length() 表明当前行已经到达末尾
-                 * strs[j].charAt(i) != c  当前行和第 0 行字符不相等
-                 * 上边两种情况返回结果
-                 */
-                if (i == strs[j].length() || strs[j].charAt(i) != c)
+                //
+                //  i == strs[j].length() 表明当前行已经到达末尾
+                //  strs[j].charAt(i) != c  当前行和第 0 行字符不相等
+                //  上边两种情况返回结果
+                //
+                if (i == strs[j].length() || strs[j].charAt(i) != c) {
                     return strs[0].substring(0, i);
+                }
             }
         }
         return strs[0];
